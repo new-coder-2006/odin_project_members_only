1bed42477b6c575b88be15e80230f3a225581f13
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-11 19:50:13 -0400

    full functionality implemented including signing out and user identities

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
 end
